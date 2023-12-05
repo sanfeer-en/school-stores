70e400f7f578f29d896d30575d705baf57c4f726
sanfeer-en <sanfi7254@gmail.com>
2023-12-05 15:21:54 +0530

    sql removed

--- storeapp/settings.py
+++ storeapp/settings.py
@@ -80,10 +80,8 @@ WSGI_APPLICATION = 'storeapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'schoolstore',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
